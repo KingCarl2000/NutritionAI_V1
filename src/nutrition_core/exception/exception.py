6116ee2fb68ce39213c8c44c437944c0bc9653d7
carl <ngvcuong282@gmail.com>
2026-08-20 22:15:48 +0700

    update pipeline architecture

--- src/nutrition_core/exception/exception.py
+++ src/nutrition_core/exception/exception.py
@@ -2,28 +2,40 @@ import sys
 from src.nutrition_core.logging import logger
 from types import ModuleType
 
-class NetworkSecurityException(Exception):
+class NutritionBaseException(Exception):
+    """Class base chứa logic lấy thông tin lỗi chung cho toàn hệ thống."""
     def __init__(self, error_message, error_details: ModuleType):
         self.error_message = error_message
         _, _, exc_tb = error_details.exc_info()
-
+        
         if exc_tb is not None:
             self.lineno = exc_tb.tb_lineno
             self.file_name = exc_tb.tb_frame.f_code.co_filename
         else:
             self.lineno = None
             self.file_name = None
-    
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-        
-if __name__=='__main__':
-    try:
-        logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed", a)
-    except Exception as e:
-           raise NetworkSecurityException(e, sys)
+        return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+            self.file_name, self.lineno, str(self.error_message)
+        )
 
+class DataPipelineException(NutritionBaseException):
+    """Lỗi chung cho quá trình luân chuyển và nạp dữ liệu."""
+    pass
 
+class DataTransformationError(DataPipelineException):
+    """
+    Lỗi phát sinh trong quá trình dbt/SQL biến đổi dữ liệu.
+    Ví dụ: Lọc nhầm các đặc trưng (features) có độ tương quan thấp nhưng 
+    lại cần thiết cho các phép biến đổi hồi quy đa thức (polynomial regression).
+    """
+    pass
+
+class DataCleansingError(DataPipelineException):
+    """
+    Lỗi phát sinh khi làm sạch dữ liệu.
+    Ví dụ: Vòng lặp thay thế giá trị null tự động ghi đè sai mã định danh 
+    của các cột phân loại (categorical columns).
+    """
+    pass
--- src/nutrition_core/logging/logger.py
+++ src/nutrition_core/logging/logger.py
@@ -2,12 +2,17 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Tách riêng ngày và thời gian chi tiết
+current_time = datetime.now()
+DATE_DIR = current_time.strftime('%m_%d_%Y')      # Ví dụ: 08_20_2026
+LOG_FILE = f"{current_time.strftime('%H_%M_%S')}.log" # Ví dụ: 20_57_42.log
 
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Tạo đường dẫn thư mục: logs/08_20_2026
+logs_path = os.path.join(os.getcwd(), "logs", DATE_DIR)
+os.makedirs(logs_path, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# File log hoàn chỉnh: logs/08_20_2026/20_57_42.log
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
